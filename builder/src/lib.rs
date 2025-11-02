5e0b5a4e00d30dd1d1857eb57bf50a4627f5e69e
Michal Maruska <mmaruska@gmail.com>
2025-11-02 21:01:21 +0100

    2TokenStream ... just new()

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -5,7 +5,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let _input = proc_macro2::TokenStream::from(input);
 
     /* transform input */
-    let output: proc_macro2::TokenStream = proc_macro2::TokenStream::default();
+    let output: proc_macro2::TokenStream = proc_macro2::TokenStream::new();
 
     proc_macro::TokenStream::from(output)
 }
