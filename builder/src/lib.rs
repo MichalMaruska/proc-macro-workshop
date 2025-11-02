c6692d3ad1db50e4dea9e999e0fc441ca79a4dbf
Michal Maruska <mmaruska@gmail.com>
2025-11-02 14:28:03 +0100

    better -- using syn

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,30 @@
 use proc_macro::TokenStream;
 
+// Better?
+// mmc: I don't understand ... https://docs.rs/proc-macro2/1.0.103/proc_macro2/
+// > If parsing with Syn, you’ll use parse_macro_input! instead to propagate
+// > parse errors correctly back to the compiler when parsing fails.
+// so it's even better?
+use syn::{parse_macro_input, Result};
+use syn::parse::{Parse, ParseStream};  // will use this trait!
+
+struct MyMacroInput {
+    /* ... */
+}
+
+impl Parse for MyMacroInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+        /* ... */
+        Ok(MyMacroInput{})
+    }
+}
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = proc_macro2::TokenStream::from(input);
+
+    // even better?
+    let _input = parse_macro_input!(input as MyMacroInput);
+    // tokens
 
     /* transform input */
     let output: proc_macro2::TokenStream = proc_macro2::TokenStream::new();
