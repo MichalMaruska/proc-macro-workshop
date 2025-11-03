cde77f2f3577b05245ce8ee1f71d3e49f9751b52
Michal Maruska <mmaruska@gmail.com>
2025-11-03 01:09:45 +0100

    extract inner type of Option and wrap conditionally in the Builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,6 +8,34 @@ use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 use quote::quote;
 
+
+// at 1:26 he simplifies a lot. to meet another requirement
+//  1:30   returns Option to avoid 2 methods.
+//  1:35  here
+
+fn ty_inner_type(ty: &syn::Type) -> Option<&syn::Type> {
+
+    if let syn::Type::Path(ref p) = ty {
+        if p.path.segments.len() != 1 || p.path.segments[0].ident != "Option" {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(ref inner_ty) = p.path.segments[0].arguments {
+            if inner_ty.args.len() != 1 {
+                return None;
+            }
+
+            let inner_ty = inner_ty.args.first().unwrap();
+
+            if let syn::GenericArgument::Type(ref t) = inner_ty {
+                return Some(t);
+            }
+        }
+    }
+    None
+}
+
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
 
@@ -32,23 +60,43 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let optionized = fields.iter().map(|f| { // over Field
         let name = &f.ident;
         let ty = &f.ty;
-        quote! { #name: std::option::Option<#ty> }
+        if ty_inner_type(ty).is_some() {
+            quote! { #name: #ty }
+        } else {
+            quote! { #name: std::option::Option<#ty> }
+        }
     });
     let build_fields = fields.iter().map(|f| {
         let name = &f.ident;
+        let ty = &f.ty;
 
-        quote! {
-            #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+        if ty_inner_type(ty).is_some() {
+            quote! {
+                #name: self.#name.clone()
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+            }
         }
     });
 
     let methods = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        quote! {
-            pub fn #name ( & mut self, #name: #ty) -> &mut Self {
-                self.#name = Some(#name);
-                self
+        if let Some(inner_ty) = ty_inner_type(ty) {
+            quote! {
+                pub fn #name ( & mut self, #name: #inner_ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                pub fn #name ( & mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
             }
         }});
 
